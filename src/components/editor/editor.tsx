58dcab4af982fc643cedd413b54cc0043d6e2117
Peter Kozari <kozaripeti@gmail.com>
2019-11-26 17:09:09 +0100

    chore: add react-router-dom and basic components

---
+++ src/components/editor/editor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Editor: React.FC = () => {
+  return (
+    <div>
+      <h1>Editor Page</h1>
+    </div>
+  )
+}
+
+export default Editor;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>This is the lifeway</h1>
+      <div>
+        <a>Registration</a>
+        <a>Sign in</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header

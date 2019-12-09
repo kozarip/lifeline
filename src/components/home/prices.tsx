34249f15871475cf9b01b522680ffb6995168f14
Peter Kozari <kozaripeti@gmail.com>
2019-12-10 00:58:08 +0100

    chore: price box correct design

--- src/components/home/prices.tsx
+++ src/components/home/prices.tsx
@@ -8,8 +8,9 @@ const Prices: React.FC = () => {
       <div className="productsBox">
         <div className="product">
           <img src=""></img>
+          <div className="greyBox"></div>
           <h4>Termék 1</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet libero non neque vulputate tincidunt. Nulla ultrices et turpis vitae blandit. Integer nec dictum ante. Nulla sodales mauris dui, sed faucibus erat condimentum sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac metus ex. Integer porta fermentum lobortis. Maecenas tincidunt turpis vitae mi elementum, non commodo augue varius. Donec in vehicula sapien.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
           <table>
             <tr><th>Paraméterek/Részletek</th></tr>
             <tr><td>Megnevezés</td></tr>
@@ -21,8 +22,9 @@ const Prices: React.FC = () => {
         </div>
         <div className="product">
           <img src=""></img>
+          <div className="greyBox"></div>
           <h4>Termék 2</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet libero non neque vulputate tincidunt. Nulla ultrices et turpis vitae blandit. Integer nec dictum ante. Nulla sodales mauris dui, sed faucibus erat condimentum sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac metus ex. Integer porta fermentum lobortis. Maecenas tincidunt turpis vitae mi elementum, non commodo augue varius. Donec in vehicula sapien.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
           <table>
             <tr><th>Paraméterek/Részletek</th></tr>
             <tr><td>Megnevezés</td></tr>
@@ -34,8 +36,9 @@ const Prices: React.FC = () => {
         </div>
         <div className="product">
           <img src=""></img>
+          <div className="greyBox"></div>
           <h4>Termék 3</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet libero non neque vulputate tincidunt. Nulla ultrices et turpis vitae blandit. Integer nec dictum ante. Nulla sodales mauris dui, sed faucibus erat condimentum sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac metus ex. Integer porta fermentum lobortis. Maecenas tincidunt turpis vitae mi elementum, non commodo augue varius. Donec in vehicula sapien.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
           <table>
             <tr><th>Paraméterek/Részletek</th></tr>
             <tr><td>Megnevezés</td></tr>

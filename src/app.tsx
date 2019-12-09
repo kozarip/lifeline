f6a91a1fb641303dc3e270444ab7ca65ac00c1e6
Peter Kozari <kozaripeti@gmail.com>
2019-12-09 19:35:30 +0100

    chore: add react-sticky for sticky header

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,10 @@ import {
   Route
 } from 'react-router-dom';
 import Header from './components/header/header';
+import {
+  StickyContainer,
+  Sticky
+} from 'react-sticky';
 
 import Introduce from './components/home/introduce';
 import HowWork from './components/home/how-work';
@@ -18,24 +22,26 @@ import './app.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-        <Header />
-        <Switch>
-          <Route path="/editor">
-            <Editor />
-          </Route>
-          <Route path="/">
-          <div id="homeContainer">
-            <Introduce />
-            <HowWork />
-            <Functions />
-            <Reviews />
-            <Prices />
-            <Contact />
+    <StickyContainer>
+      <Router>
+      <Sticky>{({ style }) => <div style={style}><Header /></div>}</Sticky>
+          <Switch>
+            <Route path="/editor">
+              <Editor />
+            </Route>
+            <Route path="/">
+            <div id="homeContainer">
+              <Introduce />
+              <HowWork />
+              <Functions />
+              <Reviews />
+              <Contact />
+              <Prices />
             </div>
-          </Route>
-        </Switch>
-    </Router>
+            </Route>
+          </Switch>
+      </Router>
+    </StickyContainer>
   );
 };
 

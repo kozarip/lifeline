4c064ac8291d3566fe548540b8aeb47e1c165ecc
Peter Kozari <kozaripeti@gmail.com>
2019-12-11 11:45:02 +0100

    style: hamburger menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import { slide as Menu } from 'react-burger-menu';
 
 const Header: React.FC = () => {
   return (
     <header>
+      <Menu className="hamburgerMenu">
+        <a className="logo" href="#">
+          <img src="#" title="Mesélő képek" />
+        </a>
+        <Link activeClass="active" to="introduce" spy={true} smooth={true} offset={-150} duration={1500}>
+            Bemutatkozás
+        </Link>
+        <Link activeClass="active" to="howWork" spy={true} smooth={true} offset={-150} duration={1500}>
+          Hogyan működik?
+        </Link>
+        <Link activeClass="active" to="functions" spy={true} smooth={true} offset={-150} duration={1500}>
+          Lehetőségek
+        </Link>
+        <Link activeClass="active" to="reviews" spy={true} smooth={true} offset={-150} duration={1500}>
+          Vélemények
+        </Link>
+        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-150} duration={1500}>
+          Kapcsolat
+        </Link>
+        <Link activeClass="active" to="products" spy={true} smooth={true} offset={-150} duration={1500}>
+          Áraink
+        </Link>
+        <a className="profileMenuPoint" href="profile">Profilom</a>
+      </Menu>
       <div className="borderKeeper"></div>
       <div className="wideHomeBox">
         <a className="logo" href="#">
           <img src="#" title="Mesélő képek" />
         </a>
+
         <ul className="menu">
           <li>
             <Link activeClass="active" to="introduce" spy={true} smooth={true} offset={-150} duration={1500}>

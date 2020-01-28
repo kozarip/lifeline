867b5b98e50a2d4fcd5e1d516125078884cbdf31
Peter Kozari <kozaripeti@gmail.com>
2020-01-28 09:02:08 +0100

    chore: logo component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,28 +3,23 @@ import { slide as HamburgerMenu } from 'react-burger-menu';
 import Scroll from 'smooth-scroll'
 
 import Menu from './menu';
+import Logo from './logo';
 
 class Header extends React.Component {
   componentDidMount() {
     const scroll = new Scroll('a[href*="#"]');
   }
 
-  menu = ""
-
   render() {
     return (
       <header>
         <HamburgerMenu className="hamburgerMenu">
-          <a className="logo" href="#">
-            <img src="#" title="Mesélő képek" />
-          </a>
+          <Logo />
           <Menu />
         </HamburgerMenu>
         <div className="borderKeeper"></div>
         <div className="wideHomeBox">
-          <a className="logo" href="#">
-            <img src="#" title="Mesélő képek" />
-          </a>
+          <Logo />
           <Menu />
         </div>
       </header>
---
+++ src/components/header/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo: React.FC = () => {
+  return (
+    <a className="logo" href="#">
+      <img src={require('../../images/logo.png')} title="Mesélő képek" />
+    </a>
+  )
+}
+
+export default Logo;

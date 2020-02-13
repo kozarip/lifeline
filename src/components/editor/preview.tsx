aa540bf599b156cbdfb1546bdc5989d72fb39782
Peter Kozari <kozaripeti@gmail.com>
2020-02-13 12:36:59 +0100

    chore: test print with jspdf and html2canvas

--- src/components/editor/preview.tsx
+++ src/components/editor/preview.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import { useSelector } from 'react-redux';
 
 const Preview: React.FC = () => {
@@ -9,13 +11,32 @@ const Preview: React.FC = () => {
   const currentThemeImageSrc = themeImageSrc ?
     themeImageSrc
     :
-    ''
+    require('images/themes/theme1.jpg');
 
   return (
-    <section id="previewContainer">
-      <img src={currentThemeImageSrc} />
-    </section>
+    <React.Fragment>
+      <section style={{position: 'relative'}} id="previewContainer">
+        <p style={{position: 'absolute', color: 'red'}}>KÉP FELIRAT</p>
+        <img src={currentThemeImageSrc} />
+      </section>
+      <button onClick={printDocument}>Nyomtatás</button>
+    </React.Fragment>
   )
 };
+
+function printDocument() {
+  const input = document.getElementById('previewContainer');
+  if (input) {
+    html2canvas(input)
+      .then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        // pdf.output('dataurlnewwindow');
+        pdf.save('download.pdf');
+      });
+  }
+
+}
 
 export default Preview;

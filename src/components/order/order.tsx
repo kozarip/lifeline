3cce8033eaa358f6f68be3cfaeb50e2688fdace4
Peter Kozari <kozaripeti@gmail.com>
2020-03-01 17:00:08 +0100

    feat: order form validation

--- src/components/order/order.tsx
+++ src/components/order/order.tsx
@@ -5,19 +5,29 @@ import { printDocument } from 'components/editor/controllers/print';
 
 const Order: React.FC = () => {
 
+  const handlePay = event => {
+    const conditions = document.getElementById('conditions');
+    if (conditions && !(conditions as HTMLInputElement).checked) {
+      alert('Kérlek fogadd el a felhasználási feltételeket')
+      event.preventDefault();
+    }
+  }
+
   return (
     <section id="editorContainer" className="wideHomeBox">
       <h2 className="componentTitle">Kérlek add meg az adataidat</h2>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       <div id="inputContainer">
-        <form>
+        <form onSubmit={handlePay}>
           <input className="narrowHomeBox textBox"
             type="text"
             name="name"
+            required
             placeholder="Neved" />
           <input className="narrowHomeBox textBox"
             type="email"
             name="email"
+            required
             placeholder="Email címed" />
           <textarea className="narrowHomeBox"
             placeholder="Megjegyzés"
@@ -36,7 +46,10 @@ const Order: React.FC = () => {
               <Link to="/conditions">Elfogadom a felhsználási feltételeket</Link>
             </label>
           </fieldset>
-          <button className="btn smallBtn">Fizetés</button>
+          <input
+            className="btn smallBtn"
+            type="submit"
+            value="Fizetés" />
         </form>
       </div>
       <button onClick={printDocument}>Nyomtatás (teszt)</button>

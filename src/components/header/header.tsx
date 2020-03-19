0242921d9383cfaf8873c4c487d0e98311dd6957
Peter Kozari <kozaripeti@gmail.com>
2020-03-19 19:03:41 +0100

    chore: header class component to functional component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import { slide as HamburgerMenu } from 'react-burger-menu';
-import Scroll from 'smooth-scroll'
 
 import Menu from './menu';
 import Logo from './logo';
 
-class Header extends React.Component {
-  componentDidMount() {
-    const scroll = new Scroll('a[href*="#"]');
-  }
-
-  render() {
-    return (
-      <header id="header">
-        <HamburgerMenu className="hamburgerMenu">
-          <Logo />
-          <Menu />
-        </HamburgerMenu>
-        <div className="borderKeeper"></div>
-        <div className="wideHomeBox">
-          <Logo />
-          <Menu />
-        </div>
-        <div id="secretPreview"></div>
-      </header>
-    );
-  }
+const Header: React.FC = () => {
+  return (
+    <header id="header">
+      <HamburgerMenu className="hamburgerMenu">
+        <Logo />
+        <Menu />
+      </HamburgerMenu>
+      <div className="borderKeeper"></div>
+      <div className="wideHomeBox">
+        <Logo />
+        <Menu />
+      </div>
+      <div id="secretPreview"></div>
+    </header>
+  );
 };
 
 export default Header;
